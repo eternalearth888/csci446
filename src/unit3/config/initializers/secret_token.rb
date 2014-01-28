57de5928e13150ffd6d609816379032a70e65dd9
Maria Deslis <mdeslis@mines.edu>
2014-01-28 16:03:08 -0700

    unit3, rails installed

---
+++ src/unit3/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unit3::Application.config.secret_key_base = 'dfa1d31e804c389f9ad1bcde0c5d1a0d318779b2aaa0459e2b29794b6b7f024c963144b699aac1094ca4acb03beac3829bff50b047f44b1842b1a83d5e0962bc'
---
+++ src/unit3/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unit3::Application.config.session_store :cookie_store, key: '_unit3_session'
