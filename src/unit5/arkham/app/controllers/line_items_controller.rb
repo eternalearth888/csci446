1f1e83de43838972b9a84836ff1a1ddab96cb0bb
Maria Deslis <mdeslis@gmail.com>
2014-02-11 19:59:30 -0700

    chapter 10 complete

--- src/unit5/arkham/app/controllers/line_items_controller.rb
+++ src/unit5/arkham/app/controllers/line_items_controller.rb
@@ -26,12 +26,10 @@ class LineItemsController < ApplicationController
   def create
   	@cart = current_cart
 	villain = Villain.find(params[:villain_id])
-	@line_item = @cart.line_items.build(:villain => villain)
-    #@line_item = LineItem.new(line_item_params)
+	@line_item = @cart.add_villain(villain.id)
 
     respond_to do |format|
       if @line_item.save
-       # format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
 	   	format.html { redirect_to(@line_item.cart, :notice => 'Line item was successfully created.') }
 		format.xml { render :xml => @line_item, :status => :created, :location => @line_item }
         format.json { render action: 'show', status: :created, location: @line_item }
