dbefa220b5f0138df9ee933157595cbca2e73199
Maria Deslis <mdeslis@gmail.com>
2014-02-17 18:36:03 -0700

    unit6, chapter 9 complete

--- src/unit6/arkham/app/models/cart.rb
+++ src/unit6/arkham/app/models/cart.rb
@@ -1,12 +1,3 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, :dependent => :destroy
-	def add_villain(villain_id)
-		current_item = line_items.find_by_villain_id(villain_id)
-		if current_item
-			current_item.quantity += 1
-		else
-			current_item = line_items.build(:villain_id => villain_id)
-		end
-	current_item
-	end
 end
